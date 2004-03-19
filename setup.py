b9f8c54de5ed687babc5153e03bb388e2f63e6ea
Denis Otkidach <denis.otkidach@gmail.com>
2004-03-19 12:34:17 +0000

    More information about distribution

--- setup.py
+++ setup.py
@@ -7,6 +7,19 @@ setup(name='QPS',
       version=qps.__version__,
       description='Q Publishing System (QPS) is a framework for custom '\
                   'Content Management Systems',
-      url='http://ppa.sf.net/',
+      url='http://ppa.sf.net/#qps',
+      maintainer='Denis S. Otkidach',
+      maintainer_email='ods@users.sf.net',
       license='Python-like',
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Information Technology',
+          'License :: OSI Approved :: Python Software Foundation License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Internet :: WWW/HTTP',
+      ],
+      download_url='http://prdownloads.sourceforge.net/ppa/'\
+                   'QPS-%s.tar.gz?download' % qps.__version__,
       packages=['qps', 'qps.qDB', 'qps.qBricks'])
