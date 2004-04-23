ff5b14f0f4666610813120c446b0399f0c0f05d5
Corvin <corvin@python.ru>
2004-04-23 11:25:21 +0000

    *** empty log message ***

---
+++ protosites/large/Handler.py
@@ -0,0 +1,28 @@
+Adapter = qps.qHTTP.getAdapter()
+class Handler(Adapter):
+    from PPA.HTTP.Errors import EndOfRequest, InternalServerError
+    
+    def handle(self, request, response):
+        self.__commands.site.clear()
+        
+        try:
+            self.__commands.handle(request, response)
+        except (self.EndOfRequest, IOError):
+            raise
+        except self.__commands.site.dbConn.OperationalError:
+            logger.exception('Unhandled exception for %s', request.uri())
+            self.reportException(response)
+        except:
+            logger.exception('Unhandled exception for %s', request.uri())
+            self.reportException(response)
+
+    def reportException(self, response):
+        raise self.InternalServerError("<pre>%s</pre>" % self.getException())
+
+    def getException(self):
+        from cStringIO import StringIO
+        import traceback
+
+        sio = StringIO()
+        traceback.print_exc(file=sio)
+        return sio.getvalue()
