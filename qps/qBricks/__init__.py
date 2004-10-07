4478ce6ff668bed009a348f79c73c5a7b15081bd
Denis Otkidach <denis.otkidach@gmail.com>
2004-10-07 13:57:12 +0000

    No need in backward compartible names anymore

--- qps/qBricks/__init__.py
+++ qps/qBricks/__init__.py
@@ -1,9 +1,5 @@
-# $Id: __init__.py,v 1.11 2004/03/16 15:56:20 ods Exp $
+# $Id: __init__.py,v 1.1.1.1 2004/03/18 15:17:17 ods Exp $
 
 '''Stream and item classes'''
-
-# XXX For compatibility
-from qSQL import SQLItem as Item, SQLStream as Stream
-from qStatic import StaticItem as ListItem, StaticStream as ListStream
 
 # vim: ts=8 sts=4 sw=4 ai et
