27174d34189cffbc1013eb852c8213a0b89af1fd
Corvin <corvin@python.ru>
2005-08-18 02:12:28 +0000

    configLogger now can reconfigure handler. This may break your code, be carefull!

--- qps/__init__.py
+++ qps/__init__.py
@@ -1,4 +1,4 @@
-# $Id: __init__.py,v 1.14 2004/10/25 13:13:41 ods Exp $
+# $Id: __init__.py,v 1.15 2005/04/05 12:23:31 ods Exp $
 
 '''Q Publishing System
 (c) 2000-2004 Sergey Barbarash, Oleg Broytmann, Pavel Barykin, Alexey
@@ -11,12 +11,13 @@ import logging
 def configLogger(
         format='%(asctime)s: %(levelname)-5s: %(name)-15s: %(message)s',
         level=logging.INFO, handler=None):
-    if not logging.root.handlers:
-        if handler is None:
-            handler = logging.StreamHandler()
-        formatter = logging.Formatter(format)
-        handler.setFormatter(formatter)
-        logging.root.addHandler(handler)
+
+    logging.root.handlers = []
+    if handler is None:
+        handler = logging.StreamHandler()
+    formatter = logging.Formatter(format)
+    handler.setFormatter(formatter)
+    logging.root.addHandler(handler)
     logging.root.setLevel(level)
 
 # vim: ts=8 sts=4 sw=4 ai et:
